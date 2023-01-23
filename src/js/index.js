8a2bf5e6695b8d799e4e0f14560f60ab0defbfb7
IagoEsdras <122841201+IagoEsdras@users.noreply.github.com>
2023-01-22 23:24:15 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+console.log('mostra o document', document);
+
+console.log(document.querySelector(".botao-trailer"));
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList. toggle("aberto");
+}
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();  
+    video.setAttribute("src", linkDoVideo);
+});
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
+
+
+
+
